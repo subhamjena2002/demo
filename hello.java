60e20227fceff48680cadd84353a16f9a71cc8bd
subham <subhamj811@gmail.com>
2023-01-05 22:16:39 +0530

    first commite

---
+++ hello.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class hello
+    {   
+        public static void main(String args[])
+            {
+                Scanner ss=new Scanner(System.in);;
+                int s=ss.nextInt();
+                System.out.println(s);
+
+            }
+    }
